40b5347fa792642384a74d4aff6d993666236e27
ash-pote <poteashleigh@gmail.com>
2022-07-19 16:08:01 +0200

    Displacement Filter & Phil Adjustment

--- js/animations.js
+++ js/animations.js
@@ -9,8 +9,8 @@ tongueImagePop.innerHTML = ""
 
 // set up a pix applicatiom
 const app = new PIXI.Application({
-    width: 220,
-    height: 220,
+    width: 170,
+    height: 170,
     transparent: true
 })
 
@@ -32,10 +32,10 @@ loader.load((loader, resources) => {
     image = new PIXI.Sprite(resources.image.texture)
     displacementImage = new PIXI.Sprite(resources.displacement.texture)
 
-    image.x = 110
-    image.y = 110
-    image.width = 240
-    image.height = 240
+    image.x = 90
+    image.y = 90
+    image.width = 180
+    image.height = 180
     image.interactive = true
 
     image.anchor.x = 0.5
